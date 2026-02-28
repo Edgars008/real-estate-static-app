8e2c10381f158a0b7ee160663a841e0d88dec1ef
Edgars008 <edgarsnaglis300@gmail.com>
2026-02-28 12:49:17 +0200

    Add scroll up

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import CssBaseline from '@mui/material/CssBaseline';
-import { BrowserRouter } from 'react-router-dom'; // ← import BrowserRouter
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import CssBaseline from "@mui/material/CssBaseline";
+import { BrowserRouter } from "react-router-dom"; // ← import BrowserRouter
+import ScrollToTop from "./components/ScrollToTop";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <CssBaseline />
-    <BrowserRouter> {/* ← wrap App with BrowserRouter */}
+    <BrowserRouter>
+      {" "}
+      {/* ← wrap App with BrowserRouter */}
+      <ScrollToTop />
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
